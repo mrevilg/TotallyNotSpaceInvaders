c17559c5152d5385047ef6264c5cfb5913931393
Stephen Clements <mrevilg@gmail.com>
2019-07-13 15:48:43 -0500

    ship module files

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
 import sys
-
 import pygame
-
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize pygame, settings, and create a screen object.
@@ -11,20 +11,20 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Totally Not Space Invaders")
+
+    # Make a ship.
+    ship = Ship(screen)
 	
     # Set the background color.
     bg_color = (230, 230, 230)
 
 	# Start main loop for the game.
     while True:
-
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
+        gf.check_events()
+        
         # Redraw the screen during each pass of the the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
