be5a3cc65db268a2ba0e3366250736c009eafa94
Stephen Clements <mrevilg@gmail.com>
2019-07-12 19:00:50 -0500

    Settings module

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """A class to store all settings for 'Totally Not Space Invaders'."""
+    def __init__(self):
+        """Initialize the game's setttings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
