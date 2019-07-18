b37ab0da36832aa9690873d644f238e8ec89e323
Stephen Clements <mrevilg@gmail.com>
2019-07-18 09:12:45 -0500

    Full High Score Rendering

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,7 +39,7 @@ def run_game():
 
 	# Start main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
